23b2f8f4502638178a5088e606edf11bc0be1728
Lou80 <lourdesramirez@yahoo.com>
2019-08-13 21:16:51 -0300

    open profile

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const usersGrid = document.getElementById('users_grid');
-const usersProfiles = document.getElementById('users_profiles');
+const userProfile = document.getElementById('user_profile');
+let users = '';
 
 String.prototype.capitalize = function() {
     return this.charAt(0).toUpperCase() + this.slice(1);
@@ -15,25 +16,40 @@ String.prototype.titleize = function() {
 }
 
 
-fetch('https://randomuser.me/api/?results=50&inc=name,location,login,picture,nat')
+fetch('https://randomuser.me/api/?results=56&inc=name,location,login,picture,nat')
 .then(res => res.json() )
 .then( data => {
-   data.results.map(u => {
+    users = data.results;
+    users.forEach(u => {
        const fullName = `${u.name.first} ${u.name.last}`.titleize();
        const city = `${u.location.city}`.titleize();
-       usersGrid.innerHTML += `<div class="user_thumbnail" id="u_${u.login.uuid}" onclick="openProfile(this)" > 
+       usersGrid.innerHTML += `<div class="user_thumbnail" id="${u.login.uuid}" onclick="openProfile(this)" > 
             <div class="img_home">
                 <img src="${u.picture.thumbnail}">
             </div>
             <div class="data_home">
-                <p>${fullName}</p>
-                <p><i class="fas fa-map-pin"></i> ${city}</p>
-                <p>${u.nat}</p>
+                <p>${fullName.trim()}</p>
+                <span><i class="fas fa-map-pin"></i> ${city}</span>
+                <span>${u.nat}</span>
             </div>
-        </div>
-        <div class="hidden profile">${fullName}</div>`;
+        </div>`;
     })
-    
 });
 
-const openProfile = (user) => user.nextSibling.nextSibling.classList.remove('hidden');
+const openProfile = (u) => {
+    const profile = users.find(user => user.login.uuid === u.id);
+    userProfile.innerHTML = `<div class="profile">
+    <span onclick="closeProfile()"><i class="far fa-times-circle"></i></span>
+    <div class="img_profile">
+        <img src="${profile.picture.large}"
+    </div>
+    </div>`;
+    usersGrid.classList.add('opacity');
+    userProfile.classList.remove('hidden');
+}
+
+const closeProfile = () => {
+    userProfile.innerHTML = '';
+    usersGrid.classList.remove('opacity');
+    userProfile.classList.add('hidden');
+}
